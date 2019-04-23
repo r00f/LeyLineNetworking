43b9bcde3a1ad4eac07d9ad22d8cec4a24fe25a1
AndCub <moritz.goeldi@zhdk.ch>
2019-04-23 16:42:37 +0200

    unitbase

---
+++ workers/unity/Assets/Scripts/Unit_BaseDataSet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LeyLineHybridECS;
+
+public class Unit_BaseDataSet : MonoBehaviour
+{
+    [Range(0, 20)]
+    public uint VisionRange;
+    public uint BaseHealth;
+    public uint UpkeepCost;
+    public uint SpawnCost;
+    public uint MovementRange;
+    public uint EnergyUpkeep;
+    public uint EnergyIncome;
+    public ECSAction BasicMove;
+    public ECSAction BasicAttack;
+    public bool IsHero;
+
+    public List<ECSAction> Actions;
+
+}
