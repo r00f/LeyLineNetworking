3232a748e56a25630009bb99e80e0d7ad4ea4654
r00f <d.wuersch@gmail.com>
2019-05-02 22:59:28 +0200

    Implemented Ragdoll handling
    - Updated animation events

--- workers/unity/Assets/Scripts/AnimationEvents.cs
+++ workers/unity/Assets/Scripts/AnimationEvents.cs
@@ -7,6 +7,43 @@ public class AnimationEvents : MonoBehaviour
     [SerializeField]
     SphereCollider WeaponCollider;
 
+    [SerializeField]
+    List<Rigidbody> RagdollRigidBodies;
+
+    [SerializeField]
+    Animator anim;
+
+    [SerializeField]
+    bool die;
+
+    public void Start()
+    {
+        /*
+        foreach (Rigidbody r in RagdollRigidBodies)
+        {
+            r.isKinematic = true;
+        }
+        */
+    }
+
+    public void Update()
+    {
+        if(die)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        anim.enabled = false;
+        foreach (Rigidbody r in RagdollRigidBodies)
+        {
+            r.isKinematic = false;
+        }
+        die = false;
+    }
+
     public void EnableWeaponCollider()
     {
         Debug.Log("EnableWeaponCollider");
@@ -18,6 +55,8 @@ public class AnimationEvents : MonoBehaviour
         Debug.Log("DisableWeaponCollider");
         WeaponCollider.enabled = false;
     }
+
+
 
 
 }
