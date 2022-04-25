7d99989b6453146f0a837f707b69f274ffc7c88a
r00f <d.wuersch@gmail.com>
2022-04-25 14:34:19 +0200

    Fixed MouseState Rightclick Reset Issue
    - Fixed a bug where rightclickevent would not be reset if user would hover a UI element

--- workers/unity/Assets/Scripts/ECSPrototype/Systems/Client/MouseStateSystem.cs
+++ workers/unity/Assets/Scripts/ECSPrototype/Systems/Client/MouseStateSystem.cs
@@ -55,7 +55,7 @@ public class MouseStateSystem : JobComponentSystem
 
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        if (eventSystem.IsPointerOverGameObject() || m_AuthoritativePlayerData.CalculateEntityCount() == 0 || m_GameStateData.CalculateEntityCount() != 1 || !Camera.main)
+        if (m_AuthoritativePlayerData.CalculateEntityCount() == 0 || m_GameStateData.CalculateEntityCount() != 1 || !Camera.main)
             return inputDeps;
 
         var gameState = m_GameStateData.GetSingleton<GameState.Component>();
@@ -66,118 +66,127 @@ public class MouseStateSystem : JobComponentSystem
         var gameStateEntity = m_GameStateData.GetSingletonEntity();
         var mapData = EntityManager.GetComponentObject<CurrentMapState>(gameStateEntity);
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var mouseLeftClick = Input.GetButtonDown("Fire1");
+        var mouseRightClick = Input.GetButtonDown("Fire2");
 
-        if (Physics.Raycast(ray, out RaycastHit hit, 100, settings.MouseRayCastLayerMask))
+        Entities.WithAll<HoveredState>().ForEach((Entity entity, int entityInQueryIndex, ref MouseState mouseState, in SpatialEntityId id, in CubeCoordinate.Component coord) =>
         {
-            Vector2 mapCenter = new Vector2(gameState.MapCenter.X, gameState.MapCenter.Y);
-            Vector2 mouseHitXZPos = (mapCenter - new Vector2(hit.point.x, hit.point.z)) * -1;
-            Vector3f posToCubeCoord = CellGridMethods.PosToCube(mouseHitXZPos);
-            Vector3 cubeCoordToWorldPos = CellGridMethods.CubeToPos(posToCubeCoord, gameState.MapCenter);
-
-            var mouseLeftClick = Input.GetButtonDown("Fire1");
-            var mouseRightClick = Input.GetButtonDown("Fire2");
-
-            bool noVisibleUnitOnHoveredCell = mapData.CoordinateCellDictionary[CellGridMethods.CubeToAxial(posToCubeCoord)].UnitOnCellId == 0 || !playerVision.CellsInVisionrange.Contains(CellGridMethods.CubeToAxial(posToCubeCoord));
-
-            if (mouseLeftClick)
+            if (mouseState.ClickEvent == 1 && !mouseLeftClick)
             {
-                m_PlayerStateSystem.ResetInputCoolDown(0.3f);
-                PlaySFXAndIncrement(playerEffects, hit.point);
+                EntityManager.RemoveComponent<ClickEvent>(entity);
+                mouseState.CurrentState = MouseState.State.Clicked;
+                mouseState.ClickEvent = 0;
             }
-            else
-                m_PlayerStateSystem.SetHoveredCoordinates(posToCubeCoord, cubeCoordToWorldPos);
 
-            EntityCommandBuffer.ParallelWriter ECBuffer = entityCommandBufferSystem.CreateCommandBuffer().AsParallelWriter();
-
-            JobHandle hoverJobHandle = Entities.WithNone<Manalith.Component>().ForEach((Entity entity, int entityInQueryIndex, ref MouseState mouseState, in SpatialEntityId id, in CubeCoordinate.Component coord) =>
+            if (mouseState.RightClickEvent == 1 && !mouseRightClick)
             {
-                if (Vector3fext.ToUnityVector(coord.CubeCoordinate) == Vector3fext.ToUnityVector(posToCubeCoord))
-                {
-                    if (mouseState.CurrentState != MouseState.State.Hovered)
-                    {
-                        ECBuffer.AddComponent(entityInQueryIndex, entity, new HoveredState());
-                        mouseState.CurrentState = MouseState.State.Hovered;
-                    }
-                }
-                else if (mouseState.CurrentState == MouseState.State.Hovered)
-                {
-                    mouseState.CurrentState = MouseState.State.Neutral;
-                    ECBuffer.RemoveComponent<HoveredState>(entityInQueryIndex, entity);
-                }
-            })
-            .Schedule(inputDeps);
+                EntityManager.RemoveComponent<RightClickEvent>(entity);
+                mouseState.RightClickEvent = 0;
+            }
+        })
+        .WithStructuralChanges()
+        .WithoutBurst()
+        .Run();
 
-            JobHandle manalithJobHandle = Entities.ForEach((Entity entity, int entityInQueryIndex, ref MouseState mouseState, in SpatialEntityId id, in Manalith.Component manalith, in CubeCoordinate.Component coord) =>
-            {
-                bool anyManalithCellHovered = false;
-
-                if (Vector3fext.ToUnityVector(coord.CubeCoordinate) == Vector3fext.ToUnityVector(posToCubeCoord))
-                {
-                    anyManalithCellHovered = true;
-                }
-
-                if (noVisibleUnitOnHoveredCell)
-                {
-                    foreach (Vector3f c in manalith.CircleCoordinatesList)
-                    {
-                        if (Vector3fext.ToUnityVector(c) == Vector3fext.ToUnityVector(posToCubeCoord) || Vector3fext.ToUnityVector(c) == Vector3fext.ToUnityVector(posToCubeCoord))
-                        {
-                            anyManalithCellHovered = true;
-                        }
-                    }
-                }
-
-                if (anyManalithCellHovered)
-                {
-                    if (mouseState.CurrentState != MouseState.State.Hovered)
-                    {
-                        ECBuffer.AddComponent(entityInQueryIndex, entity, new HoveredState());
-                        mouseState.CurrentState = MouseState.State.Hovered;
-                    }
-                }
-                else if (mouseState.CurrentState == MouseState.State.Hovered)
-                {
-                    ECBuffer.RemoveComponent<HoveredState>(entityInQueryIndex, entity);
-                    mouseState.CurrentState = MouseState.State.Neutral;
-                }
-            })
-            .WithoutBurst()
-            .Schedule(hoverJobHandle);
-
-            JobHandle clickJobHandle = Entities.WithAll<HoveredState>().ForEach((Entity entity, int entityInQueryIndex, ref MouseState mouseState, in SpatialEntityId id, in CubeCoordinate.Component coord) =>
+        if (!eventSystem.IsPointerOverGameObject() && IsMouseOverGameWindow)
+        {
+            Entities.WithAll<HoveredState>().ForEach((Entity entity, int entityInQueryIndex, ref MouseState mouseState, in SpatialEntityId id, in CubeCoordinate.Component coord) =>
             {
                 if (mouseRightClick)
                 {
-                    ECBuffer.AddComponent(entityInQueryIndex, entity, new RightClickEvent());
+                    EntityManager.AddComponent<RightClickEvent>(entity);
                     mouseState.RightClickEvent = 1;
                 }
 
                 if (mouseLeftClick)
                 {
-                    ECBuffer.AddComponent(entityInQueryIndex, entity, new ClickEvent());
+                    EntityManager.AddComponent<ClickEvent>(entity);
                     mouseState.ClickEvent = 1;
                 }
-
-                if (mouseState.ClickEvent == 1 && !mouseLeftClick)
-                {
-                    ECBuffer.RemoveComponent<ClickEvent>(entityInQueryIndex, entity);
-                    mouseState.CurrentState = MouseState.State.Clicked;
-                    mouseState.ClickEvent = 0;
-                }
-
-                if (mouseState.RightClickEvent == 1 && !mouseRightClick)
-                {
-                    ECBuffer.RemoveComponent<RightClickEvent>(entityInQueryIndex, entity);
-                    mouseState.RightClickEvent = 0;
-                }
             })
-            .Schedule(manalithJobHandle);
+            .WithStructuralChanges()
+            .WithoutBurst()
+            .Run();
 
-            return clickJobHandle;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, 100, settings.MouseRayCastLayerMask))
+            {
+                Vector2 mapCenter = new Vector2(gameState.MapCenter.X, gameState.MapCenter.Y);
+                Vector2 mouseHitXZPos = (mapCenter - new Vector2(hit.point.x, hit.point.z)) * -1;
+                Vector3f posToCubeCoord = CellGridMethods.PosToCube(mouseHitXZPos);
+                Vector3 cubeCoordToWorldPos = CellGridMethods.CubeToPos(posToCubeCoord, gameState.MapCenter);
+                EntityCommandBuffer.ParallelWriter ECBuffer = entityCommandBufferSystem.CreateCommandBuffer().AsParallelWriter();
+
+                bool noVisibleUnitOnHoveredCell = mapData.CoordinateCellDictionary[CellGridMethods.CubeToAxial(posToCubeCoord)].UnitOnCellId == 0 || !playerVision.CellsInVisionrange.Contains(CellGridMethods.CubeToAxial(posToCubeCoord));
+
+                if (mouseLeftClick)
+                {
+                    m_PlayerStateSystem.ResetInputCoolDown(0.3f);
+                    PlaySFXAndIncrement(playerEffects, hit.point);
+                }
+                else
+                    m_PlayerStateSystem.SetHoveredCoordinates(posToCubeCoord, cubeCoordToWorldPos);
+
+                JobHandle hoverJobHandle = Entities.WithNone<Manalith.Component>().ForEach((Entity entity, int entityInQueryIndex, ref MouseState mouseState, in SpatialEntityId id, in CubeCoordinate.Component coord) =>
+                {
+                    if (Vector3fext.ToUnityVector(coord.CubeCoordinate) == Vector3fext.ToUnityVector(posToCubeCoord))
+                    {
+                        if (mouseState.CurrentState != MouseState.State.Hovered)
+                        {
+                            ECBuffer.AddComponent(entityInQueryIndex, entity, new HoveredState());
+                            mouseState.CurrentState = MouseState.State.Hovered;
+                        }
+                    }
+                    else if (mouseState.CurrentState == MouseState.State.Hovered)
+                    {
+                        mouseState.CurrentState = MouseState.State.Neutral;
+                        ECBuffer.RemoveComponent<HoveredState>(entityInQueryIndex, entity);
+                    }
+                })
+                .Schedule(inputDeps);
+
+                JobHandle manalithJobHandle = Entities.ForEach((Entity entity, int entityInQueryIndex, ref MouseState mouseState, in SpatialEntityId id, in Manalith.Component manalith, in CubeCoordinate.Component coord) =>
+                {
+                    bool anyManalithCellHovered = false;
+
+                    if (Vector3fext.ToUnityVector(coord.CubeCoordinate) == Vector3fext.ToUnityVector(posToCubeCoord))
+                    {
+                        anyManalithCellHovered = true;
+                    }
+
+                    if (noVisibleUnitOnHoveredCell)
+                    {
+                        foreach (Vector3f c in manalith.CircleCoordinatesList)
+                        {
+                            if (Vector3fext.ToUnityVector(c) == Vector3fext.ToUnityVector(posToCubeCoord) || Vector3fext.ToUnityVector(c) == Vector3fext.ToUnityVector(posToCubeCoord))
+                            {
+                                anyManalithCellHovered = true;
+                            }
+                        }
+                    }
+
+                    if (anyManalithCellHovered)
+                    {
+                        if (mouseState.CurrentState != MouseState.State.Hovered)
+                        {
+                            ECBuffer.AddComponent(entityInQueryIndex, entity, new HoveredState());
+                            mouseState.CurrentState = MouseState.State.Hovered;
+                        }
+                    }
+                    else if (mouseState.CurrentState == MouseState.State.Hovered)
+                    {
+                        ECBuffer.RemoveComponent<HoveredState>(entityInQueryIndex, entity);
+                        mouseState.CurrentState = MouseState.State.Neutral;
+                    }
+                })
+                .WithoutBurst()
+                .Schedule(hoverJobHandle);
+
+                return manalithJobHandle;
+            }
         }
-        else
-            return inputDeps;
+        return inputDeps;
     }
 
     void PlaySFXAndIncrement(PlayerEffects playerEffects, Vector3 position)
@@ -194,4 +203,6 @@ public class MouseStateSystem : JobComponentSystem
         else
             playerEffects.CurrentMouseClickIndex = 0;
     }
+
+    bool IsMouseOverGameWindow { get { return !(0 > Input.mousePosition.x || 0 > Input.mousePosition.y || Screen.width < Input.mousePosition.x || Screen.height < Input.mousePosition.y); } }
 }
