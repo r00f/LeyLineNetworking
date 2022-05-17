a3febd9f43ec7029169aa8c68abafad4fe15c025
r00f <d.wuersch@gmail.com>
2022-05-17 17:17:36 +0200

    Started Implementing Controls Panel

--- workers/unity/Assets/unity-ui-extensions/Scripts/Utilities/UILineConnector.cs
+++ workers/unity/Assets/unity-ui-extensions/Scripts/Utilities/UILineConnector.cs
@@ -1,4 +1,4 @@
-﻿/// Credit Alastair Aitchison
+/// Credit Alastair Aitchison
 /// Sourced from - https://bitbucket.org/UnityUIExtensions/unity-ui-extensions/issues/123/uilinerenderer-issues-with-specifying
 
 namespace UnityEngine.UI.Extensions
@@ -12,14 +12,10 @@ namespace UnityEngine.UI.Extensions
         // The elements between which line segments should be drawn
         public RectTransform[] transforms;
         private Vector2[] previousPositions;
-        private RectTransform canvas;
-        private RectTransform rt;
         private UILineRenderer lr;
 
         private void Awake()
         {
-            canvas = GetComponentInParent<RectTransform>().GetParentCanvas().GetComponent<RectTransform>();
-            rt = GetComponent<RectTransform>();
             lr = GetComponent<UILineRenderer>();
         }
 
@@ -44,31 +40,11 @@ namespace UnityEngine.UI.Extensions
                 if (!updateLine) return;
             }
 
-            // Get the pivot points
-            Vector2 thisPivot = rt.pivot;
-            Vector2 canvasPivot = canvas.pivot;
-
-            // Set up some arrays of coordinates in various reference systems
-            Vector3[] worldSpaces = new Vector3[transforms.Length];
-            Vector3[] canvasSpaces = new Vector3[transforms.Length];
             Vector2[] points = new Vector2[transforms.Length];
 
-            // First, convert the pivot to worldspace
             for (int i = 0; i < transforms.Length; i++)
             {
-                worldSpaces[i] = transforms[i].TransformPoint(thisPivot);
-            }
-
-            // Then, convert to canvas space
-            for (int i = 0; i < transforms.Length; i++)
-            {
-                canvasSpaces[i] = canvas.InverseTransformPoint(worldSpaces[i]);
-            }
-
-            // Calculate delta from the canvas pivot point
-            for (int i = 0; i < transforms.Length; i++)
-            {
-                points[i] = new Vector2(canvasSpaces[i].x, canvasSpaces[i].y);
+                points[i] = new Vector2(transforms[i].anchoredPosition.x, transforms[i].anchoredPosition.y);
             }
 
             // And assign the converted points to the line renderer
